99f702060d4a25c98f822cdb71f5a45a60b63829
Percy Nizamov <Mysticgummibear@gmail.com>
2021-03-19 23:03:02 -0600

    logic framework

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,70 @@
-import javax.swing.JFrame;
-import java.awt.EventQueue; 
+//import statements
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Calculator extends JFrame{
+    
+    private JFrame frame;
+
+    
+    public Calculator(){
+        initUI(); 
+    }
+
+    private void initUI(){
+        
+        frame.setSize(400,400); 
+
+        var one = new JButton("1");
+        var two = new JButton("2");
+        var three = new JButton("3"); 
+        var four = new JButton("4");
+        var five = new JButton("5");
+        var six = new JButton("6");
+        var seven = new JButton("7");
+        var eight = new JButton("8");
+        var nine = new JButton("9");
+        var plus = new JButton("+");
+        var minus = new JButton("-");
+        var multiply = new JButton("*");
+        var divide = new JButton("/"); 
+
+        one.addActionListener(placeholder()); 
+        two.addActionListener();
+    }
+
+    public void placeholder(){
+        numOne = one; 
+    }; 
+
+    public void setNum(){
+        
+        if(plus == true){
+            ans = numOne+numTwo; 
+
+        }
+
+        if(minus == true){
+            ans = numOne + numTwo; 
+        }
+
+        //etc
+         
+    }
 
 
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            var calculator = new Calculator();
+            calculator.setVisible(true);
+        });
+
+        if(numOne = true && plus == true || minus == true || multiply == true || division == true){
+            setNum(); 
+        }
+    }
 
 }
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## GUI CALCULATOR 
 
-``A calculator made by Percy Nizamov, for his APCSA computer science class to do basic opperations.`` 
+### I'm honestly just burnt out, if you see this message, that means that it is not complete but I tried my hardest to get some framework in. If not well, future Percy worked on this, and this will be a meer memory in commits. 
+
+
+```A calculator made by Percy Nizamov, for his APCSA computer science class to do basic opperations.``` 
